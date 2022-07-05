30e75b97bb96003921d20e557ba97c590b1e0f9b
wyddmw <spyderzsy@gmail.com>
2022-07-05 15:05:38 +0800

    test new device

--- tools/demo.py
+++ tools/demo.py
@@ -14,6 +14,7 @@ except:
 import numpy as np
 import torch
 
+
 from pcdet.config import cfg, cfg_from_yaml_file
 from pcdet.datasets import DatasetTemplate
 from pcdet.models import build_network, load_data_to_gpu
